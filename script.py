da618a7655a1885da3c8563ece5dbbc3d0e39962
Ivan <ivan@localhost.localdomain>
2021-01-12 12:36:37 +0300

    .

--- script.py
+++ script.py
@@ -10,8 +10,8 @@ username_input=browser.find_element_by_id("username")
 password_input=browser.find_element_by_id("auth_password")
 login_button=browser.find_element_by_xpath("//*[@id=\"login\"]/div[3]/input")
 
-username_input.send_keys("kopytkoi@eurotorg.by")
-password_input.send_keys("HK4p12jcX1")
+username_input.send_keys("YOUR_LOGIN")
+password_input.send_keys("YOUR_PASSWORD")
 login_button.click()
 sleep(1.2)
 
@@ -19,4 +19,4 @@ browser.get('https://e-plus.by/#/mvc/publications/admin/addpublication/rubric_id
 sleep(15)
 
 
-browser.close()
+# browser.close()
